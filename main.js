6d7d5cd7037f9136369cffbceb93bf433c105b70
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-10 14:31:34 -0500

    use seconds for everything

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 function update() {
-    var now = new Date();
-    var secs = Math.floor(now / 1000);
-    timestamp.innerText = secs.toLocaleString();
+    const now = Math.floor(new Date() / 1000);
+    timestamp.innerText = now.toLocaleString();
     
     countdown.innerText = timeToMilestone(now);
 }
 
 function timeToMilestone(now, milestone) {
     if (!milestone) {
-    milestone = Math.ceil(now / 1e11) * 1e11;
+        milestone = Math.ceil(now / 1e8) * 1e8;
     }
 
-    var periods = [
-    [365*24*60*60*1000, 'year'],
-    [24*60*60*1000, 'day'],
-    [60*60*1000, 'hour'],
-    [60*1000, 'minute'],
-    [1000, 'second']
+    const periods = [
+        [365*24*60*60, 'year'],
+        [24*60*60, 'day'],
+        [60*60, 'hour'],
+        [60, 'minute'],
+        [1, 'second']
     ];
 
-    var delta = milestone - now;
-    var deltaString = '';
+    let delta = milestone - now;
+    let deltaString = '';
     for (let i = 0; i < periods.length; i++) {
-    var count = Math.floor(delta / periods[i][0])
-    if (count) {
-        if (deltaString.length) {
-        deltaString += ' ';
+        let count = Math.floor(delta / periods[i][0])
+        if (count) {
+            if (deltaString.length) {
+                deltaString += ' ';
+            }
+
+            deltaString += `${count} ${periods[i][1]}`;
+            if (count > 1) {
+                deltaString += 's'
+            }
+
+            delta -= periods[i][0] * count;
         }
-
-        deltaString += `${count} ${periods[i][1]}`;
-        if (count > 1) {
-        deltaString += 's'
-        }
-
-        delta -= periods[i][0] * count;
-    }
     }
 
-    deltaString += ' until ' + Math.floor(milestone / 1000).toLocaleString();
+    deltaString += ' until ' + Math.floor(milestone).toLocaleString();
     return deltaString;
 }
 
